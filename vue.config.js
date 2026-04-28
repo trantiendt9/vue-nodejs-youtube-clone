9f0399ae45d9a317152d33dc1be45a5ff23c54d7
trantiendt9 <36979269+trantiendt9@users.noreply.github.com>
2026-04-28 14:58:51 +0700

    Update vue.config.js with additional settings
    Added publicPath and productionSourceMap settings.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
+  transpileDependencies: [
+    'vuetify'
+  ],
+  // Đảm bảo các file static (js, css) được nạp đúng từ thư mục gốc
+  publicPath: '/',
+  
+  // Tắt source map khi build production để giảm dung lượng
+  productionSourceMap: false
 }
